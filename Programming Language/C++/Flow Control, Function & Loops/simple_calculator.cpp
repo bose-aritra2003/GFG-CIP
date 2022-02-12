475c4f4640bd472b482f6a8de31d76e6dfff3522
Aritra Bose <aritrabose2003@gmail.com>
2022-02-13 00:19:45 +0530

    Add files via upload

---
+++ Programming Language/C++/Flow Control, Function & Loops/simple_calculator.cpp
@@ -0,0 +1,25 @@
+//SIMPLE CALCULATOR
+/* There will be 3 integer inputs
+ * First input will be for which type of operation
+ * 1 -> Addition, 2 -> Subtraction, 3 -> Multiplication
+ * Next 2 inputs will be the operands
+ * If for the first input user enters anything other than 1/2/3
+   the program will show invalid input
+ */
+
+#include <iostream>
+int main() {
+    int n, x, y;
+    std::cin >> n >> x >> y;
+    switch(n) {
+        case 1: std::cout << (x + y);
+                break;
+        case 2: std::cout << (x - y);
+                break;
+        case 3: std::cout << (x * y);
+                break;
+        default:
+                std::cout << "Invalid Input!\n";
+    }
+    return 0;
+}
