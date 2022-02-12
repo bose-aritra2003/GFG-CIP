e10625a9a84d1cea2cd364fd27cdccb6c51daed0
Aritra Bose <aritrabose2003@gmail.com>
2022-02-13 00:25:54 +0530

    Add files via upload

---
+++ Programming Language/C++/Flow Control, Function & Loops/leap_year.cpp
@@ -0,0 +1,20 @@
+//LEAP YEAR
+/* Conditions are:
+ * 1) Divisible by 4
+ * 2) But not divisible by 100
+ * 3) Unless divisible by 400
+ */
+
+#include <iostream>
+int main() {
+    int n{};
+    std::cin >> n;
+
+    if(n % 4 == 0 && n % 100 != 0)
+        std::cout << "Leap year";
+    else if(n % 400 == 0)
+        std::cout << "Leap year";
+    else
+        std::cout << "Not a leap year";
+    return 0;
+}
