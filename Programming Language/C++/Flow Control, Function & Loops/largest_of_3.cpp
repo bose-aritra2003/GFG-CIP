936013385c56eff095b9be0f2008a1ade4865757
Aritra Bose <aritrabose2003@gmail.com>
2022-02-13 00:10:49 +0530

    Add files via upload

---
+++ Programming Language/C++/Flow Control, Function & Loops/largest_of_3.cpp
@@ -0,0 +1,17 @@
+//LARGEST OF 3 NUMBERS
+
+#include <iostream>
+
+int main() {
+    std::cout << "Enter 3 number seperated by spaces: ";
+    int a{}, b{}, c{};
+    std::cin >> a >> b >> c;
+
+    if(a >= b && a >= c)
+        std::cout << a;
+    else if(b >= a && b >= c)
+        std::cout << b;
+    else
+        std::cout << c;
+    return 0;
+}
