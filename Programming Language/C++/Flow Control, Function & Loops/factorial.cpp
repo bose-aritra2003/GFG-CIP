d829c7fd87984a0999743fc2144970ee29f39087
Aritra Bose <aritrabose2003@gmail.com>
2022-02-21 00:00:09 +0530

    Add files via upload

---
+++ Programming Language/C++/Flow Control, Function & Loops/factorial.cpp
@@ -0,0 +1,20 @@
+// Factorial of a number
+
+/* Factorial of 5 = 1 * 2 * 3 * 4 * 5 */
+
+#include <iostream>
+
+int factorial(int n) {
+    int f = 1;
+    for(int i = 1; i <= n; i++) {
+        f *= i;
+    }
+    return f;
+}
+
+int main() {
+    std::cout << "Enter an integer to find out its factorial: ";
+    int number{};
+    std::cin >> number;
+    factorial(number);
+}
