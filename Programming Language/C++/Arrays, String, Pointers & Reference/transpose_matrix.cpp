c8e9091b92cb287fead23fc7d8d4c58368ef07d5
Aritra Bose <aritrabose2003@gmail.com>
2022-02-26 12:53:42 +0530

    Add files via upload

---
+++ Programming Language/C++/Arrays, String, Pointers & Reference/transpose_matrix.cpp
@@ -0,0 +1,40 @@
+// Program to do transpose of a matrix
+
+/* Diagonal elements do not change.
+ * Only elements at r != c and r <= c
+ * are swapped like r, c = c, r
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void transposeOf(auto &arr) {
+    for(int r = 0; r < arr.size(); r++)
+        for(int c = 0; c < arr[r].size(); c++) {
+            if(r != c and r <= c)
+                swap(arr[r][c], arr[c][r]);
+        }
+}
+
+void print(auto &arr) {
+    for(int r = 0; r < arr.size(); r++) {
+        for (int c = 0; c < arr[r].size(); c++) {
+            cout << arr[r][c] << ' ';
+        }
+        cout << '\n';
+    }
+}
+
+int main() {
+    vector<vector<int>> arr = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+    };
+    print(arr);
+    cout << '\n';
+    transposeOf(arr);
+    cout << '\n';
+    print(arr);
+}
