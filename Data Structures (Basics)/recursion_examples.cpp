d7c02fa7707d18b9955e674feae1edc993f40a18
Aritra Bose <aritrabose2003@gmail.com>
2022-06-27 19:05:19 +0530

    Update recursion_examples.cpp

--- Data Structures (Basics)/recursion_examples.cpp
+++ Data Structures (Basics)/recursion_examples.cpp
@@ -152,6 +152,45 @@ void towerOfHanoi(int n, char source = 'A', char destination = 'C', char aux = '
      * Recurrence relation: T(n) = 2T(n-1) + Θ(1)
      * Time complexity: O(2^n)
      */
+
+    /*
+        Tower of Hanoi is a mathematical puzzle where we have three rods and n disks.
+        The objective of the puzzle is to move the entire stack to another rod, 
+        obeying the following simple rules:
+
+        Only one disk can be moved at a time.
+        Each move consists of taking the upper disk from one of the stacks and
+        placing it on top of another stack i.e.
+        a disk can only be moved if it is the uppermost disk on a stack.
+        No disk may be placed on top of a smaller disk.
+
+        Approach :
+
+        Let rod 1 = 'A', rod 2 = 'B', rod 3 = 'C'.
+        
+        An example with 2 disks :
+        Step 1 : Shift first disk from 'A' to 'B'.
+
+        Step 2 : Shift second disk from 'A' to 'C'.
+
+        Step 3 : Shift first disk from 'B' to 'C'.
+        An example with 3 disks :
+        Step 1 : Shift first disk from 'A' to 'C'.
+        Step 2 : Shift second disk from 'A' to 'B'.
+        Step 3 : Shift first disk from 'C' to 'B'.
+
+        Step 4 : Shift third disk from 'A' to 'C'.
+
+        Step 5 : Shift first disk from 'B' to 'A'.
+        Step 6 : Shift second disk from 'B' to 'C'.
+        Step 7 : Shift first disk from 'A' to 'C'.
+        (Notice the gaps)
+        The pattern here is :
+        - Shift 'n-1' disks from 'A' to 'B', using C.
+        - Shift last disk from 'A' to 'C'.
+        - Shift 'n-1' disks from 'B' to 'C', using A.
+     */
+    
     if(n == 1) {
         cout << "Move disk 1 from " << source << " to " << destination << '\n';
         return;
